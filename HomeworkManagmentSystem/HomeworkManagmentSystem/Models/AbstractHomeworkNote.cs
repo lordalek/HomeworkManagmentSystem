26cd113995be904df7956bf79b7b19aa311e8d66
Aleksander Pettersen <alekspett@gmail.com>
2013-03-04 21:45:50 +0100

    Initial commit

---
+++ HomeworkManagmentSystem/HomeworkManagmentSystem/Models/AbstractHomeworkNote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HomeworkManagmentSystem.Models
+{
+    public abstract class AbstractHomeworkNote
+    {
+        public string HomeworkNote { get; set; }
+    }
+}
---
+++ HomeworkManagmentSystem/HomeworkManagmentSystem/Models/AbstractUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeworkManagmentSystem.Models
+{
+    public abstract class AbstractUser
+    {
+        /// <summary>
+        /// An user's ID
+        /// </summary>
+        public long UserID { get; set; }
+    }
+}
